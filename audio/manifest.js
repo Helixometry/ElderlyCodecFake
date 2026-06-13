0a3bd8f8d73fdc874a9b6154057f83e854a96b08
Girish <girishproject28@gmail.com>
2026-06-13 14:05:32 +0530

    final

---
+++ audio/manifest.js
@@ -0,0 +1,37 @@
+window.MANIFEST_RAW = [
+  {
+    codec: "snac_24khz",
+    gt_id: "Elderly0014S0046W0001",
+    source: "SeniorTalk (Chinese)",
+    gt_url: "audio/REAL/Elderly0014S0046W0001.wav",
+    gen_url: "audio/FAKE/Elderly0014S0046W0001_hubertsiuzdak_snac_24khz.wav"
+  },
+  {
+    codec: "snac_24khz",
+    gt_id: "Elderly0014S0046W0002",
+    source: "SeniorTalk (Chinese)",
+    gt_url: "audio/REAL/Elderly0014S0046W0002.wav",
+    gen_url: "audio/FAKE/Elderly0014S0046W0002_hubertsiuzdak_snac_24khz.wav"
+  },
+  {
+    codec: "snac_24khz",
+    gt_id: "Elderly0014S0046W0003",
+    source: "SeniorTalk (Chinese)",
+    gt_url: "audio/REAL/Elderly0014S0046W0003.wav",
+    gen_url: "audio/FAKE/Elderly0014S0046W0003_hubertsiuzdak_snac_24khz.wav"
+  },
+  {
+    codec: "snac_24khz",
+    gt_id: "Elderly0014S0046W0004",
+    source: "SeniorTalk (Chinese)",
+    gt_url: "audio/REAL/Elderly0014S0046W0004.wav",
+    gen_url: "audio/FAKE/Elderly0014S0046W0004_hubertsiuzdak_snac_24khz.wav"
+  },
+  {
+    codec: "snac_24khz",
+    gt_id: "Elderly0014S0046W0005",
+    source: "SeniorTalk (Chinese)",
+    gt_url: "audio/REAL/Elderly0014S0046W0005.wav",
+    gen_url: "audio/FAKE/Elderly0014S0046W0005_hubertsiuzdak_snac_24khz.wav"
+  }
+];
